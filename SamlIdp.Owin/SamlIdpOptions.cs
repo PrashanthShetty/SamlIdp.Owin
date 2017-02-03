5f775e7eef0afc6efe3b3b7e4fd6fbbd33945f74
PrashanthShetty <prashanths.shetty@gmail.com>
2017-02-02 18:30:03 -0800

    Added SP Validation Options
    Check for incoming SAML Request with valid SP

--- SamlIdp.Owin/SamlIdpOptions.cs
+++ SamlIdp.Owin/SamlIdpOptions.cs
@@ -4,7 +4,8 @@
     using System.Security.Cryptography.X509Certificates;
     using Kentor.AuthServices.WebSso;
     using Microsoft.Owin.Security.DataProtection;
-
+    using System;
+     
     public class SamlIdpOptions
     {
         public SamlIdpOptions()
@@ -12,7 +13,8 @@
             BindingType = Saml2BindingType.HttpPost;
             ServiceProviders = new List<ServiceProvider>();
         }
-
+        public bool ValidateServiceProvider { get; set; }
+        public Func<string, string> ServiceProviderValidationMessage { get; set; }
         public string AuthenticationType { get; set; }
         public X509Certificate2 SigningCertificate { get; set; }
         public Saml2BindingType BindingType { get; set; }
